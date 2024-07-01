35d1628d0bc04bca101209468c2d1835cda8c7fd
GHOST-Aram <owinoojala@gmail.com>
2024-07-01 13:37:00 +0300

    feat: Create ProductDataAccess class
         - Extend the GenericDataAccess class with the new ProductDataAccess

---
+++ src/products/data-acess/data-access.ts
@@ -0,0 +1,6 @@
+import { GenericDataAccess } from "../../z-library/bases/generic-data-access";
+import { Product, ProductModel } from "./model";
+
+export class ProductDataAccess extends GenericDataAccess<ProductModel, Product>{
+    
+}
