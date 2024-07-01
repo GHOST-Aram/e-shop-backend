7ccaf3a57fd9f31d4981a43e526784f1cd6f7a77
GHOST-Aram <owinoojala@gmail.com>
2024-07-01 13:27:52 +0300

    feat: Create Product Model
          - Create and export product interface, model and hydrated product document

---
+++ src/products/data-acess/model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, HydratedDocument, Model, Schema } from 'mongoose'
+
+export interface Product extends Document {
+	productName: string
+	currentPrice: number
+	previousPrice: number
+	description: string
+	selectedFile: string
+}
+
+export type ProductModel = Model<Product>
+
+const productSchema: Schema = new Schema<Product, ProductModel>({
+    productName: { 
+		type: String, 
+		required: true 
+	},
+    currentPrice: { 
+		type: Number, 
+		required: true 
+	},
+    previousPrice: { 
+		type: Number, 
+		required: true 
+	},
+    description: { 
+		type: String, 
+		required: true 
+	},
+    selectedFile: { 
+		type: String, 
+		required: true 
+	}, // assuming the file is stored as a path or URL
+})
+
+export type HydratedProductDoc = HydratedDocument<Product>
+
+export const Product: ProductModel = mongoose.model<Product, ProductModel>(
+  'Product', productSchema)
