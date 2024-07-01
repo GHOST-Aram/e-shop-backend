08d53ed4716a14cf39b129a31686634f6afd5e13
GHOST-Aram <owinoojala@gmail.com>
2024-07-01 14:08:51 +0300

    chore: Write tests for products POST route
          - Test for Method not allowed, status code 405 incase the client
            tries to create products with their self defined ids
          - Test for Invalid input, status 400 incase the client sends a request
            with invalid data
          - Test for Successful response, status 200.

---
+++ src/products/tests/mocks/test-data.ts
@@ -0,0 +1,7 @@
+export const productData = {
+
+}
+
+export const badProductData = {
+    
+}
---
+++ src/products/tests/post.test.ts
@@ -0,0 +1,33 @@
+import request from "supertest"
+import { app } from "./config/app.test.config"
+import { describe, test } from "@jest/globals"
+import { assert } from "../../z-library/testing/response-assertion"
+import { productData, badProductData } from "./mocks/test-data"
+
+
+describe('Rentals POST Route', () => {
+    test('Responds with Method not allowed, status 405: User defined Ids not allowed', 
+        async() =>{
+            const response = await request(app).post('/rentals/64c9e4f2df7cc072af2ac9e')
+                .send(productData)
+
+            assert.respondsWithMethodNotAllowed(response)
+        }
+    )
+
+    test('Responds with validation errors, status 400: Invalid input', 
+        async() =>{
+            const response = await request(app).post('/rentals').send(badProductData)
+
+            assert.respondsWithBadRequest(response)
+            assert.respondsWithValidationErrors(response)
+        }
+    )
+
+    test('Responds with created resource, status 201: Post success', 
+        async() =>{
+            const response = await request(app).post('/rentals').send(productData)
+            assert.respondsWithCreatedResource(response)
+        }
+    )
+})
