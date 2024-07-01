e8541f5e2b87c133a284154fe281af23239ff4b9
GHOST-Aram <owinoojala@gmail.com>
2024-07-01 13:41:08 +0300

    feat: Create Product controller class
         - Create a new ProductController class that extends GenericController class

---
+++ src/products/controllers/controller.ts
@@ -0,0 +1,6 @@
+import { GenericController } from "../../z-library/bases/generic-controller";
+import { ProductDataAccess } from "../data-acess/data-access";
+
+export class ProductsController extends GenericController<ProductDataAccess>{
+
+}
