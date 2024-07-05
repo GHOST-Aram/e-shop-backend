0b08726c3fca769f8b72fe991be088601f7aa52b
GHOST-Aram <owinoojala@gmail.com>
2024-07-05 13:01:22 +0300

    chore: Connect application to the Database

--- src/_config/app.ts
+++ src/_config/app.ts
@@ -1,9 +1,22 @@
 import express, { Application } from "express"
 import { Server } from "../z-library/server/server"
+import { Connection } from "../z-library/db/connection"
 import 'dotenv/config'
 
 const app: Application = express()
 const server = new Server(app)
+
+const mongoDbUri = process.env.MONGODB_URI
+if(mongoDbUri){
+    try {
+        new Connection(mongoDbUri)
+    } catch (error) {
+        console.log(error)
+    }
+} else {
+    console.log('Database connection string is null.')
+}
+
 
 server.useJSONPayloads()
 server.allowCrossOriginResourceSharing()
